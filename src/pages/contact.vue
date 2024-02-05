d37000c58b7f40673eb429ead980dc0e44718543
antran2704 <phamtrangiaan27@gmail.com>
2024-02-05 22:10:05 +0700

    update

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -1,69 +1,216 @@
 <template>
-    <div>
-        <!-- BREADCRUMB AREA START -->
-        <Breadcrumb title="Liên Hệ" :list="[{ label: 'Home', path: '/' }, { label: 'Liên Hệ' }]" />
-        <!-- CONTACT ADDRESS AREA START -->
-        <div class="ltn__contact-address-area mb-60">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-3">
-                        <div class="ltn__contact-address-item ltn__contact-address-item-4 box-shadow">
-                            <div class="ltn__contact-address-icon">
-                                <i class="icon-location-pin"></i>
-                            </div>
-                            <h3>Email Address</h3>
-                            <p>info@webmail.com <br>
-                                jobs@webexample.com</p>
-                        </div>
-                    </div>
-                    <div class="col-lg-3">
-                        <div class="ltn__contact-address-item ltn__contact-address-item-4 box-shadow">
-                            <div class="ltn__contact-address-icon">
-                                <i class="icon-phone"></i>
-                            </div>
-                            <h3>Phone Number</h3>
-                            <p>+1234-567 890 <br>
-                                +09876-543 210</p>
-                        </div>
-                    </div>
-                    <div class="col-lg-3">
-                        <div class="ltn__contact-address-item ltn__contact-address-item-4 box-shadow">
-                            <div class="ltn__contact-address-icon">
-                                <i class="icon-envelope"></i>
-                            </div>
-                            <h3>Email & Web</h3>
-                            <p>info@webmail.com <br>
-                                jobs@webexample.com</p>
-                        </div>
-                    </div>
-                    <div class="col-lg-3">
-                        <div class="ltn__contact-address-item ltn__contact-address-item-4 box-shadow">
-                            <div class="ltn__contact-address-icon">
-                                <i class="icon-speedometer"></i>
-                            </div>
-                            <h3>Opening Hours</h3>
-                            <p>Fri to Wed: 6:00 Am to 8:00 Pm <br>
-                                Thursday - Off</p>
-                        </div>
+    <Seo title="Liên hệ" description="Description contact" :canonical="`${runtimeConfig.public.domain}/about`"
+        picture="product.Picture" />
+
+
+    <Breadcrumb title="Liên Hệ" :list="[{ label: 'Home', path: '/' }, { label: 'Liên Hệ' }]" />
+
+    <!-- ABOUT US AREA START -->
+    <div class="ltn__about-us-area pt-90 pb-70">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 align-self-center">
+                    <div class="about-us-info-wrap">
+                        <h2 class="mb-30 font-weight-6">Fiama - Cửa hàng bán quần áo Online</h2>
+                        <p class="mb-10">Với khơi nguồn từ lòng đam mê thời trang, khát khao mang đến cái đẹp cho tất cả phụ
+                            nữ và hơn thế
+                            nữa là mong muốn được góp phần tạo dựng hình ảnh mới lạ cho ngành công nghiệp thời trang Việt
+                            Nam, Fiama đã tập trung đầu tư vào chất lượng và kiểu dáng sản phẩm để thương hiệu Fiama Fashion
+                            trở thành một cái tên gần gũi hơn với khách hàng.
+                            Với phương châm "BRIGHTEN YOUR DAY", Fiama mong muốn mang lại cho khách hàng những sản phẩm tốt
+                            nhất để các bạn không chỉ thể hiện cá tính bản thân mà còn lan toả nguồn năng lượng tích cực,
+                            mạnh mẽ đến xung quanh.</p>
+                        <p>Các mẫu thiết kế của Fiama luôn mang đậm phong cách phóng khoáng và tự do. Sự kết hợp hài hoà
+                            giữa
+                            hai dòng thời trang basic và trendy khiến các thiết kế vừa mang tính tiện dụng, phù hợp với
+                            nhiều bối cảnh hằng ngày mà vẫn bắt kịp xu hướng mốt của năm.
+                            Không chỉ đem đến cho khách hàng những sản phẩm mang phong cách và kiểu dáng độc đáo mà còn giới
+                            thiệu đến khách hàng những sản phẩm đạt chất lượng tốt nhất. Các mẫu thiết kế được kiểm duyệt kĩ
+                            càng từ khâu chọn chất liệu, dựng mẫu và hoàn thiện.
+                            Chính vì thế, những thiết kế đến tay khách hàng luôn là những sản phẩm tinh tế nhất. Với đội ngũ
+                            chuyên nghiệp cùng sự kết hợp chặt chẽ và thống nhất của ban lãnh đạo, thương hiệu thời trang
+                            Fiama hy vọng sẽ đáp ứng được sự yêu mến và tin cậy của quý khách hàng về dòng sản phẩm thời
+                            trang chất lượng cao.</p>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- GOOGLE MAP AREA START -->
-        <div class="ltn_google-map-area mb-60">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="google-map">
-                            <iframe
-                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9334.271551495209!2d-73.97198251485975!3d40.668170674982946!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25b0456b5a2e7%3A0x68bdf865dda0b669!2sBrooklyn%20Botanic%20Garden%20Shop!5e0!3m2!1sen!2sbd!4v1590597267201!5m2!1sen!2sbd"
-                                width="100%" height="100%" frameborder="0" allowfullscreen="true" aria-hidden="false"
-                                tabindex="0"></iframe>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- CONTACT MESSAGE AREA START -->
     </div>
+    <!-- ABOUT US AREA END -->
+
+    <!-- VIDEO AREA START -->
+    <div class="ltn__video-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="ltn__video-bg-img ltn__video-popup-height-5 section-bg-3--- bg-image pt-170 pb-150"
+                        data-bs-bg="/img/bg/2.jpg" id="ltnr_video">
+                        <a class="ltn__video-icon-1"
+                            href="https://www.youtube.com/embed/J6oDTn_ENto?autoplay=1&amp;showinfo=0"
+                            data-rel="lightcase:myCollection">
+                            <i class="icon-control-play"></i>
+                        </a>
+                        <br>
+                        <div class="ltn__video-info d-none">
+                            <h1 class="white-color">APPS AWESOME VIDEO</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- VIDEO AREA END -->
+
+    <!-- TESTIMONIAL AREA START -->
+    <div class="testimonial-area pt-95 pb-80">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="section-title-area text-center">
+                        <h1 class="section-title section-title-border">Khách hàng</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-8 offset-lg-2 col-md-12  col-sm-12 col-xs-12">
+                    <!-- Testimonial Wrap -->
+                    <div class="ltn__testimonial-7-wrap text-center">
+                        <div class="ltn__testimonial-7-image-slider">
+                            <div class="testimonial-image">
+                                <img src="/img/team/11.jpg" alt="Images of Testimonial" />
+                            </div>
+                            <div class="testimonial-image">
+                                <img src="/img/team/12.jpg" alt="Images of Testimonial" />
+                            </div>
+                            <div class="testimonial-image">
+                                <img src="/img/team/13.jpg" alt="Images of Testimonial" />
+                            </div>
+                            <div class="testimonial-image">
+                                <img src="/img/team/14.jpg" alt="Images of Testimonial" />
+                            </div>
+                        </div>
+                        <!--End Testimonial Wrap -->
+                        <!--Start Testimonial Text Slider -->
+                        <div class="ltn__testimonial-7-text-slider slick-arrow-1">
+                            <div class="testimonial-info">
+                                <p class="text-capitalize"> áo rất dày dặn, chất liệu vải rất tốt, đường may không bị thưa,
+                                    vì là giá rẻ nên không thể đòi hỏi gì thêm quá tốt cho 1 chiếc ảo</p>
+                                <h5>Antran dev</h5>
+                                <h6>Khách hàng</h6>
+                            </div>
+                            <div class="testimonial-info">
+                                <p>Áo nhung tăm chất liệu đẹp tuyệt vời thì mình sẽ còn ủng hộ tiếp nha shop, rất ưng ý.</p>
+                                <h5>Antran</h5>
+                                <h6>Khách hàng</h6>
+                            </div>
+                            <div class="testimonial-info">
+                                <p>Giao nhanh, sản phẩm chất lượng, màu áo đậm hơn so với trong hình nhưng vẫn xinh, chất
+                                    vải ổn, nói chung xịn lắm không có gì để chê</p>
+                                <h5>Antran 2</h5>
+                                <h6>Khách hàng</h6>
+                            </div>
+                            <div class="testimonial-info">
+                                <p>Áo quá xuất sắc luôn. Áo đép giao hàng nhanh shop uy tín lắm nha cả nhà. Nói chung mọi
+                                    người nên mua đi chơi tết lần sau sẽ ủng hộ nữa nhá 5 sao</p>
+                                <h5>Antran 3</h5>
+                                <h6>Khách hàng</h6>
+                            </div>
+                        </div>
+                        <!--End Testimonial Text Slider -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- TESTIMONIAL AREA END -->
+
+    <!-- BANNER AREA START -->
+    <div class="ltn__banner-area ">
+        <div class="container">
+            <div class="row">
+                <BannerDiscount title="Banner Image" alt="Banner Image" src="/img/banner/10.jpg" col="col-12" />
+            </div>
+        </div>
+    </div>
+    <!-- BANNER AREA END -->
+
+    <!-- FEATURE AREA START ( Feature - 3) -->
+    <div class="ltn__contact-address-area pt-30 pb-30">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3">
+                    <div class="ltn__contact-address-item ltn__contact-address-item-4 box-shadow">
+                        <div class="ltn__contact-address-icon">
+                            <i class="icon-location-pin"></i>
+                        </div>
+                        <h3>Điaj chỉ</h3>
+                        <p>123 Nguyễn Kiệm, P.3, Gò Vấp, TP.HCM
+                        </p>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="ltn__contact-address-item ltn__contact-address-item-4 box-shadow">
+                        <div class="ltn__contact-address-icon">
+                            <i class="icon-phone"></i>
+                        </div>
+                        <h3>Số điện thoại</h3>
+                        <p>+1234-567 890 <br>
+                            +09876-543 210</p>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="ltn__contact-address-item ltn__contact-address-item-4 box-shadow">
+                        <div class="ltn__contact-address-icon">
+                            <i class="icon-envelope"></i>
+                        </div>
+                        <h3>Email & Web</h3>
+                        <p>info@webmail.com <br>
+                            jobs@webexample.com</p>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="ltn__contact-address-item ltn__contact-address-item-4 box-shadow">
+                        <div class="ltn__contact-address-icon">
+                            <i class="icon-speedometer"></i>
+                        </div>
+                        <h3>Giờ mở cửa</h3>
+                        <p>Thứ 2 đến thứ 6: <br> 6:00 đến 20:00
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- FEATURE AREA END -->
+
+    <!-- BRAND LOGO AREA START -->
+    <Brand />
+    <!-- BRAND LOGO AREA END -->
 </template>
+
+<script setup>
+const runtimeConfig = useRuntimeConfig();
+const feedbacks = [
+    {
+        name: "Antran dev",
+        role: "Khách hàng",
+        content: "áo rất dày dặn, chất liệu vải rất tốt, đường may không bị thưa, vì là giá rẻ nên không thể đòi hỏi gì thêm quá tốt cho 1 chiếc ảo"
+    },
+    {
+        name: "Antran",
+        role: "Khách hàng",
+        content: "Áo nhung tăm chất liệu đẹp tuyệt vời thì mình sẽ còn ủng hộ tiếp nha shop, rất ưng ý."
+    },
+    {
+        name: "Antran 2",
+        role: "Khách hàng",
+        content: "Giao nhanh, sản phẩm chất lượng, màu áo đậm hơn so với trong hình nhưng vẫn xinh, chất vải ổn, nói chung xịn lắm không có gì để chê"
+    },
+    {
+        name: "Antran 3",
+        role: "Khách hàng",
+        content: "Áo quá xuất sắc luôn. Áo đép giao hàng nhanh shop uy tín lắm nha cả nhà. Nói chung mọi người nên mua đi chơi tết lần sau sẽ ủng hộ nữa nhá 5 sao"
+    },
+]
+
+</script>
